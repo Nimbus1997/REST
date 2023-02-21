e32c1a722676c1b861f3a050075bc712c760fcd5
Nimbus1997 <blue8smile@naver.com>
2023-02-21 07:38:08 +0000

    miv1 23.02.21 resent scatter

--- models/networks.py
+++ models/networks.py
@@ -175,6 +175,9 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
     elif netG == 'resnet_4blocks':
         net = ResnetGenerator(
             input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=4)
+    elif netG == 'resnet_4blocks_scattering':
+        net = ResnetGenerator_with_scattering(
+            input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=4, input_size=input_size)
     # elif netG == 'resnet_3blocks':
     #     net = ResnetGenerator(
     #         input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=3)
@@ -514,6 +517,77 @@ class ResnetGenerator(nn.Module):
         """Standard forward"""
         return self.model(input)
 
+
+class ResnetGenerator_with_scattering(nn.Module):
+    """Resnet-based generator that consists of Resnet blocks between a few downsampling/upsampling operations.
+
+    We adapt Torch code and idea from Justin Johnson's neural style transfer project(https://github.com/jcjohnson/fast-neural-style)
+    """
+
+    def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=6, padding_type='reflect', input_size=512):
+        """Construct a Resnet-based generator
+
+        Parameters:
+            input_nc (int)      -- the number of channels in input images
+            output_nc (int)     -- the number of channels in output images
+            ngf (int)           -- the number of filters in the last conv layer
+            norm_layer          -- normalization layer
+            use_dropout (bool)  -- if use dropout layers
+            n_blocks (int)      -- the number of ResNet blocks
+            padding_type (str)  -- the name of padding layer in conv layers: reflect | replicate | zero
+        """
+        assert(n_blocks >= 0)
+        super(ResnetGenerator_with_scattering, self).__init__()
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+        
+        self.input_size= input_size
+        self.scatteringlayer = scatter_transform(input_nc, out_channels=1,size=input_size,level=1,kind=1, scattering_attention=True  )
+
+
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(input_nc+1, ngf, kernel_size=7,
+                           padding=0, bias=use_bias),
+                 norm_layer(ngf),
+                 nn.ReLU(True)]
+
+        n_downsampling = 2
+        for i in range(n_downsampling):  # add downsampling layers
+            mult = 2 ** i
+            model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),
+                      norm_layer(ngf * mult * 2),
+                      nn.ReLU(True)]
+
+        mult = 2 ** n_downsampling
+        for i in range(n_blocks):       # add ResNet blocks
+
+            model += [ResnetBlock(ngf * mult, padding_type=padding_type,
+                                  norm_layer=norm_layer, use_dropout=use_dropout, use_bias=use_bias)]
+
+        for i in range(n_downsampling):  # add upsampling layers
+            mult = 2 ** (n_downsampling - i)
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+                                         kernel_size=3, stride=2,
+                                         padding=1, output_padding=1,
+                                         bias=use_bias),
+                      norm_layer(int(ngf * mult / 2)),
+                      nn.ReLU(True)]
+        model += [nn.ReflectionPad2d(3)]
+        model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        model += [nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, input):
+        """Standard forward"""
+        scattering_output = self.scatteringlayer(input)
+        scattering_output = interpolate(scattering_output, size=(self.input_size, self.input_size), mode='bilinear')
+
+        catinput = torch.cat([input, scattering_output],1)
+
+        return self.model(catinput)
 
 class ResnetBlock(nn.Module):
     """Define a Resnet block"""
